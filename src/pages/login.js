819a4b8dfc81e2e6a66c436495c6978508d4a603
Saerom Bang <saerombang11@gmail.com>
2021-07-09 21:44:59 +0900

    Added Login Button

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+import { isValid } from 'date-fns';
 import { React, useState, useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
@@ -10,7 +11,8 @@ export default function Login() {
   const [emailAddress, setEmailAddress] = useState(' ');
   const [password, setPassword] = useState(' ');
   const [error, setError] = useState(' ');
-  const isValid = password === '' || emailAddress === '';
+
+  const isInvalid = setPassword === '' || setEmailAddress === '';
 
   const handleLogin = () => {};
 
@@ -20,14 +22,16 @@ export default function Login() {
 
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <div className="flex w-3/5"></div>
-      <img
-        src="/images/avatars/users/iphone-with-profile.jpg"
-        alt="iPhone with Instagram"
-      />
+      <div className="flex w-3/5">
+        <img
+          src="/images/avatars/users/iphone-with-profile.jpg"
+          alt="iPhone with Instagram"
+        />
+      </div>
       <div className="flex flex-col w-2/5">
         <h1 className="flex justify-center w-full">
           <img
+            img
             src="/images/avatars/users/logo.png"
             alt="Instagram"
             className="mt-2 w-6/12 mb-4"
@@ -39,8 +43,24 @@ export default function Login() {
             aria-label="Enter your email address"
             type="text"
             placeholder="Email address"
-            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2"
+            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+            onChange={({ target }) => setEmailAddress(target.value)}
           ></input>
+          <input
+            aria-label="Enter your password"
+            type="text"
+            placeholder="password"
+            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+            onChange={({ target }) => setPassword(target.value)}
+          ></input>
+          <button
+            disabled={isInvalid}
+            type="submit"
+            className={`bg-blue-500 text-white w-full rounded h-8 font-bold
+            ${isInvalid && 'opacity-50'}`}
+          >
+            Log In
+          </button>
         </form>
       </div>
     </div>
