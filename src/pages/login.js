e7e58571a4708e1f4a7f4a8effc0b7fe30b2874e
Saerom Bang <saerombang11@gmail.com>
2021-07-13 19:40:23 +0900

    Added routes

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,6 +3,7 @@ import { isValid } from 'date-fns';
 import { React, useState, useContext, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
+import * as ROUTES from '../constants/routes';
 
 export default function Login() {
   const history = useHistory();
@@ -14,7 +15,17 @@ export default function Login() {
 
   const isInvalid = password === '' || emailAddress === '';
 
-  const handleLogin = () => {};
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      await firebase.auth().signInWithEmailandPassword(emailAddress, password);
+      history.push(ROUTES.DASHBOARD);
+    } catch (error) {
+      setEmailAddress('');
+      setPassword('');
+      setError(error.message);
+    }
+  };
 
   useEffect(() => {
     document.title = 'Login - Instagram';
