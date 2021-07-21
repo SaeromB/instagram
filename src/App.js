7853a9d058be8ad923743431a19c9679e98dc1e6
Saerom Bang <saerombang11@gmail.com>
2021-07-21 21:11:54 +0900

    Added user in context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
+// import UserContext from './context/user';
+// import useAuthListener from './hooks/use-auth-listener';
 
 const Login = lazy(() => import('./pages/login'));
 const SignUp = lazy(() => import('./pages/sign-up'));
@@ -8,14 +10,17 @@ const Dashboard = lazy(() => import('./pages/dashboard'));
 const NotFound = lazy(() => import('./pages/not-found'));
 
 function App() {
+  // const { user } = useAuthListener();
   return (
     <Router>
       <Suspense fallback={<p>Loading...</p>}>
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login} />
           <Route path={ROUTES.SIGN_UP} component={SignUp} />
+          <Route path={ROUTES.DASHBOARD} component={Dashboard}>
+            <Dashboard />
+          </Route>
           <Route component={NotFound} />
-          <Route path={ROUTES.DASHBOARD} component={Dashboard} />
         </Switch>
       </Suspense>
     </Router>
--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext, React } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import FirebaseContext from '../context/firebase';
 
 export default function useAuthListener() {
