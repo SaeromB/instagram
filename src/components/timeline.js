4b7019cb2b17474d2b554b8b4f6ffc9fd65015c3
Saerom Bang <saerombang11@gmail.com>
2021-07-31 02:27:51 +0900

    Added timeline

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,9 +1,20 @@
-import React from 'react';
+iimport { useContext } from 'react';
+import Skeleton from 'react-loading-skeleton';
+import LoggedInUserContext from '../context/logged-in-user';
+import usePhotos from '../hooks/use-photos';
+import Post from './post';
 
 export default function Timeline() {
+  const { user } = useContext(LoggedInUserContext);
+  const { photos } = usePhotos(user);
+
   return (
     <div className="container col-span-2">
-      <p>I am Timeline</p>
+      {!photos ? (
+        <Skeleton count={4} width={640} height={500} className="mb-5" />
+      ) : (
+        photos.map((content) => <Post key={content.docId} content={content} />)
+      )}
     </div>
   );
 }
